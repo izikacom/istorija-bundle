499c8b0dd69c071a81476daca4c37e23217c2597
Thomas Tourlourat <thomas@tourlourat.com>
2024-07-09 13:51:24 +0200

    bump to symfony 5.4

--- src/Command/DebugHandlerCommand.php
+++ src/Command/DebugHandlerCommand.php
@@ -3,32 +3,36 @@
 namespace Dayuse\IstorijaBundle\Command;
 
 use Dayuse\IstorijaBundle\ServiceHandlerRegistry;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class DebugHandlerCommand extends ContainerAwareCommand
+class DebugHandlerCommand extends Command
 {
+    protected static $defaultName = 'istorija:debug:handler';
+
+    public function __construct(
+        private readonly ServiceHandlerRegistry $serviceHandlerRegistry,
+    ) {
+        parent::__construct(self::$defaultName);
+    }
+
     protected function configure()
     {
         $this
-            ->setName('istorija:debug:handler')
             ->setDescription('Debug command and event handlers');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $io = new SymfonyStyle($input, $output);
-        
-        /** @var ServiceHandlerRegistry $serviceHandlerRegistry */
-        $serviceHandlerRegistry = $this->getContainer()->get('istorija.service_handler_registry');
-        
+
         $io->title('Configured service event handlers');
-        $io->listing($serviceHandlerRegistry->getServiceEventHandlers());
+        $io->listing($this->serviceHandlerRegistry->getServiceEventHandlers());
 
         $io->title('Configured service command handlers');
-        $io->listing(array_keys($serviceHandlerRegistry->getCommandHandlers()));
+        $io->listing(array_keys($this->serviceHandlerRegistry->getCommandHandlers()));
 
         return 0;
     }
